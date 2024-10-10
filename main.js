f8e2d70f539023a273a5f8b4b8e6e49235f0ea42
tarek7salamaa <tarek7salamaa@gmail.com>
2024-10-10 15:14:26 +0300

    Some Enhancement

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
+// https://api.currencyfreaks.com/v2.0/rates/latest?apikey=cc9271d395d24c77950745ef050c04a3
+
+let amount = document.querySelector(".amount"),
+  egp = document.querySelector(".egp span"),
+  sar = document.querySelector(".sar span");
+
 fetch(
-  "https://api.currencyfreaks.com/v2.0/rates/latest?apikey=5382649875144788b0e1cda479bcfd41"
+  "https://api.currencyfreaks.com/v2.0/rates/latest?apikey=cc9271d395d24c77950745ef050c04a3"
 )
   .then((result) => {
     let myData = result.json();
     return myData;
   })
-  .then((currency) => {
-    let amount = document.querySelector(".amount").innerHTML;
-    let egp = document.querySelector(".egp span");
-    let sar = document.querySelector(".sar span");
-
-    egp.innerHTML = Math.round(amount * currency.rates["EGP"]);
-    sar.innerHTML = Math.round(amount * currency.rates["SAR"]);
-
-    console.log(egp);
-    console.log(sar);
+  .then((myData) => {
+    egp.innerHTML = myData.rates.EGP;
+    sar.innerHTML = myData.rates.SAR;
   });
